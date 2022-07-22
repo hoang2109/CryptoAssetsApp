f52cec9d36613c816154342f6b523a6f207e44d9
Hoang Nguyen <hoang2109@gmail.com>
2022-07-22 23:28:04 +0800

    chore: create `CryptoAssetsUI` framework

---
+++ CryptoAssetsUI/CryptoAssetsUI.h
@@ -0,0 +1,18 @@
+//
+//  CryptoAssetsUI.h
+//  CryptoAssetsUI
+//
+//  Created by Hoang Nguyen on 22/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CryptoAssetsUI.
+FOUNDATION_EXPORT double CryptoAssetsUIVersionNumber;
+
+//! Project version string for CryptoAssetsUI.
+FOUNDATION_EXPORT const unsigned char CryptoAssetsUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoAssetsUI/PublicHeader.h>
+
+
