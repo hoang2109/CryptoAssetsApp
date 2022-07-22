c63247fb15167a0e54873af417f4a06e73f392b8
Hoang Nguyen <hoang2109@gmail.com>
2022-07-22 23:30:55 +0800

    chore: create `CryptoAssetsCore` framework

---
+++ CryptoAssetsCore/CryptoAssetsCore.h
@@ -0,0 +1,18 @@
+//
+//  CryptoAssetsCore.h
+//  CryptoAssetsCore
+//
+//  Created by Hoang Nguyen on 22/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CryptoAssetsCore.
+FOUNDATION_EXPORT double CryptoAssetsCoreVersionNumber;
+
+//! Project version string for CryptoAssetsCore.
+FOUNDATION_EXPORT const unsigned char CryptoAssetsCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoAssetsCore/PublicHeader.h>
+
+
